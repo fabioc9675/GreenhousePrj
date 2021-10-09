12ad843fdd53020fd7034fdebeafeece6d5f9105
Fabian Castaño <fabioc9675@gmail.com>
2021-10-09 00:52:55 -0500

    Error and exception control added

--- Software/greenhouse-pyt/dataReader.py
+++ Software/greenhouse-pyt/dataReader.py
@@ -1,9 +1,30 @@
+from os import error
+import sys
 import serial
 
-# make the connection with the device
-dev = serial.Serial("/dev/cu.usbserial-0001", 115200)
 
-while(1):
-    data = dev.read_until()
+def main():
+    # make the connection with the device
 
-    print(data)
+    try:
+        dev = serial.Serial("/dev/cu.usbserial-0001", 115200)
+
+        while(1):
+            data = dev.read_until()
+
+            print(data)
+
+    except error:
+        print("Port not found")
+
+    except KeyboardInterrupt:
+        print("Program interrupted")
+        dev.close()
+
+    finally:
+        sys.exit(0)
+
+
+if __name__ == '__main__':
+
+    main()
