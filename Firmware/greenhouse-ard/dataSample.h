4d38abe0cd96a87b937a2f1e2b6179908ad88e70
Fabian Castaño <fabioc9675@gmail.com>
2021-10-12 23:40:23 -0500

    Udapte in generation of data to test communication

--- Firmware/greenhouse-ard/dataSample.h
+++ Firmware/greenhouse-ard/dataSample.h
@@ -1,35 +1,24 @@
 /************************************************************
- * File: initializer.h                                      *
+ * File: dataSample.h                                       *
  * Author: Fabian Castano                                   *
  * Project: Greenhouse-prj                                  *
  * Version: 1.0.0                                           *
  * Organization: University of Antioquia                    *
  ************************************************************/
 
-#ifndef INITIALIZER_H
-#define INITIALIZER_H
+#ifndef DATASAMPLE_H
+#define DATASAMPLE_H
 
 /* ********************************************************************
  * **** INCLUDES 
  * ********************************************************************/
 #include <Arduino.h>
 
+#include "initializer.h"
+
 /* ********************************************************************
  * **** DEFINES 
  * ********************************************************************/
-#define INSTITUTION "JFK"
-#define GREENHOUSE_NUM 1
-
-// Commands
-#define READ_CMD 'r'
-#define ALIVE_CMD 'a'
-
-// data sample constant
-#define TIME_TO_SEC 10      // need to count until 10 to rise a second
-#define SECONDS_TO_SAMPLE 2 // seconds to obtain a sample
-#define SECONDS_TO_LED 1    // seconds to toogle led
-#define SAMPLE_PERIOD (TIME_TO_SEC * SECONDS_TO_SAMPLE)
-#define LED_PERIOD (TIME_TO_SEC * SECONDS_TO_LED)
 
 /* ********************************************************************
  * **** GLOBAL VARIABLES 
@@ -42,11 +31,10 @@
 /* ********************************************************************
  * **** PROTOTYPES 
  * ********************************************************************/
-// function to initialize text variables
-void init_TextPayload(void);
+void dataRandGenerator(void);
 
 /* ********************************************************************
  * **** FUNCTIONS 
  * ********************************************************************/
 
-#endif /* INITIALIZER_H */
+#endif /* DATASAMPLE_H */
--- Firmware/greenhouse-ard/dataSample.ino
+++ Firmware/greenhouse-ard/dataSample.ino
@@ -1,39 +1,35 @@
 /************************************************************
- * File: initializer.h                                      *
+ * File: dataSample.ino                                     *
  * Author: Fabian Castano                                   *
  * Project: Greenhouse-prj                                  *
  * Version: 1.0.0                                           *
  * Organization: University of Antioquia                    *
  ************************************************************/
 
-#ifndef INITIALIZER_H
-#define INITIALIZER_H
-
 /* ********************************************************************
  * **** INCLUDES 
  * ********************************************************************/
-#include <Arduino.h>
+#include "dataSample.h"
 
 /* ********************************************************************
  * **** DEFINES 
  * ********************************************************************/
-#define INSTITUTION "JFK"
-#define GREENHOUSE_NUM 1
-
-// Commands
-#define READ_CMD 'r'
-#define ALIVE_CMD 'a'
-
-// data sample constant
-#define TIME_TO_SEC 10      // need to count until 10 to rise a second
-#define SECONDS_TO_SAMPLE 2 // seconds to obtain a sample
-#define SECONDS_TO_LED 1    // seconds to toogle led
-#define SAMPLE_PERIOD (TIME_TO_SEC * SECONDS_TO_SAMPLE)
-#define LED_PERIOD (TIME_TO_SEC * SECONDS_TO_LED)
 
 /* ********************************************************************
- * **** GLOBAL VARIABLES 
+ * **** EXTERN VARIABLES 
  * ********************************************************************/
+// Variables to storage locally the information
+extern float temp_env_val;
+extern float mois_env_val;
+extern float radi_env_val;
+extern float temp_e1_val;
+extern float temp_e2_val;
+extern float temp_e3_val;
+extern float temp_e4_val;
+extern float humi_e1_val;
+extern float humi_e2_val;
+extern float humi_e3_val;
+extern float humi_e4_val;
 
 /* ********************************************************************
  * **** LOCAL VARIABLES 
@@ -42,11 +38,22 @@
 /* ********************************************************************
  * **** PROTOTYPES 
  * ********************************************************************/
-// function to initialize text variables
-void init_TextPayload(void);
 
 /* ********************************************************************
  * **** FUNCTIONS 
  * ********************************************************************/
-
-#endif /* INITIALIZER_H */
+void dataRandGenerator(void)
+{
+    // create data random
+    temp_env_val = random(2000, 3500) / 100.0;
+    mois_env_val = random(4000, 9000) / 100.0;
+    radi_env_val = random(1000, 2500) / 100.0;
+    temp_e1_val = random(2000, 3500) / 100.0;
+    temp_e2_val = random(2000, 3500) / 100.0;
+    temp_e3_val = random(2000, 3500) / 100.0;
+    temp_e4_val = random(2000, 3500) / 100.0;
+    humi_e1_val = random(4000, 9000) / 100.0;
+    humi_e2_val = random(4000, 9000) / 100.0;
+    humi_e3_val = random(4000, 9000) / 100.0;
+    humi_e4_val = random(4000, 9000) / 100.0;
+}
