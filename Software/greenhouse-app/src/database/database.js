07e07fb5d814d443c1c4fd6ed98df08220400adb
Fabian Castano <fabioc9675@gmail.com>
2021-10-16 17:26:35 -0500

    Addition of database response, mongoose watch when data is inserted

--- Software/greenhouse-app/src/database/database.js
+++ Software/greenhouse-app/src/database/database.js
@@ -9,4 +9,21 @@ mongoose
   .then((db) => console.log("Database is connected"))
   .catch((err) => console.error(err));
 
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connected");
+
+  // Setting change streams in the database
+  console.log("Setting change streams");
+  const greenhouseChangeStream = connection.collection("greenhouses").watch();
+
+  greenhouseChangeStream.on("change", (change) => {
+    switch (change.operationType) {
+      case "insert":
+        console.log("data inserted");
+    }
+  });
+});
+
 module.exports = mongoose;
